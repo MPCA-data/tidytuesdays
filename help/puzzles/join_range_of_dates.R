8256f20d13967464a78b3376c7faedd3e97cedb8
air <dKvale@users.noreply.github.com>
2020-09-01 09:30:15 -0500

    Update join_range_of_dates.R

--- help/puzzles/join_range_of_dates.R
+++ help/puzzles/join_range_of_dates.R
@@ -1,8 +1,14 @@
+# How do I find if a wildfire happened during one of the monitoring times?
+
+# I want to join the wildfire dates to the monitoring table, but the monitoring dates
+# are a range: a start and an end date. I want to know if a fire occurred on any of the 
+# days in that window.
+
 library(dplyr)
 library(tidyr)
 
 
-# Generate demo data
+# Some demo data
 two_years <- seq(as.Date("2018-09-02"), 
                  as.Date("2020-08-01"), 1)
 
@@ -15,12 +21,12 @@ monitor_times <- tibble(mon_id     = rep(1:3, 4),
 
 # Add list of monitoring dates as a column
 
-## This WONT work!
+## This WONT work
 monitor_times <- monitor_times %>%
                  rowwise() %>%    # Only look at 1 row at a time
                  mutate(date_window = seq(start_date, end_date, 1))
 
-## This will
+## This will work
 ## ADD list() around seq()
 monitor_times <- monitor_times %>%
                  rowwise() %>%
