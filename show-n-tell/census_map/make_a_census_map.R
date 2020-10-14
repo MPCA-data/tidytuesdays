a25db07bc45b5c7af19097bf84b3dd3bac408ba3
kmellick <kmellick@users.noreply.github.com>
2020-10-14 10:11:58 -0500

    Add files via upload

--- show-n-tell/census_map/make_a_census_map.R
+++ show-n-tell/census_map/make_a_census_map.R
@@ -4,14 +4,20 @@
 
 ###I always load this R package, just because it has most of what I need to process my data.
 ##Enter the tidyverse!
+
 library(tidyverse)
 
 ###First I want the shapes of the census block groups. I use the tigris package for that. The tigris package is awesome because of its hex sticker (see it in your chat). We all need wall hangings with that image on it, am I right?
+
 library(tigris)
 block_groups <- block_groups(state = 'MN', cb = TRUE) %>% select(GEOID, geometry)
 options(tigris_use_cache = TRUE)
 
 plot(block_groups)
+
+
+##Just in case the tiger shapefile website is down, I have saved the shapefile.
+##block_groups <- read_sf("C:/Users/kmell/Desktop/air_assessment_section_meeting/block_groups.shp")
 
 ##A census block group is a geography where between 600 - 3000 people live.
 ##Now we want to pull populations of kids within the block groups.
@@ -36,6 +42,7 @@ kiddos <- get_acs(geography = "block group",
   group_by(GEOID) %>%
   summarise(kiddo_population = sum(kiddo_population))
 
+##Just in case the census site goes down, I have saved the census data here.
 ##kiddos <- read_csv("C:/Users/kmell/Desktop/air_assessment_section_meeting/census_data.csv")
 
 ##Pull in some MNRISKS air pollution risk scores. 
@@ -44,22 +51,29 @@ risks <- read_csv("C:/Users/kmell/Desktop/air_assessment_section_meeting/2014_BG
 
 ##Join all the data
 
-risks <- left_join(risks, kiddos, by = c("geoid" = "GEOID"))
+risks_kiddos <- left_join(risks, kiddos, by = c("geoid" = "GEOID"))
 
-risks <- risks %>%
+risks_kiddos <- risks_kiddos %>%
   rowwise() %>%
   mutate(cool_label = paste("air score = ", combined_air_score, ", population of kids = ", kiddo_population))
 
 ##Let R know that there is geographic information in the table
 library(sf)
-risks <- st_as_sf(risks, crs = 4269, sf_column_name = "geometry")
+risks_kiddos <- st_as_sf(risks_kiddos, crs = 4269, sf_column_name = "geometry")
 
 ##Put the data in a map
 library(leaflet)
 
-leaflet(risks) %>%
+leaflet(risks_kiddos) %>%
   addPolygons(
     weight = 1,
     color = "grey",
     fillColor = ~colorQuantile("YlOrRd", combined_air_score)(combined_air_score), 
     label = ~cool_label)
+
+##Save the data as a shapefile
+write_sf(risks_kiddos, "C:/Users/kmell/Desktop/air_assessment_section_meeting/risks_kiddos.shp")
+
+##This script is stored here: https://github.com/MPCA-data/tidytuesdays/tree/master/show-n-tell/census_map
+
+##Thanks for listening!!
