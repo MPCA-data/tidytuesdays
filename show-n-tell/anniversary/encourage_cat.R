717fcc857480b0a06fa828d2e05d834632ade805
air <dKvale@users.noreply.github.com>
2021-01-25 22:41:49 -0600

    Update encourage_cat.R

--- show-n-tell/anniversary/encourage_cat.R
+++ show-n-tell/anniversary/encourage_cat.R
@@ -1,38 +1,45 @@
 # An encouragement kitty that is sometimes punny
 ## Add your name for a personalized message
 
+# source("https://raw.githubusercontent.com/MPCA-data/tidytuesdays/master/show-n-tell/anniversary/encourage_cat.R")
+# encourage_cat()
+
+
 encourage_cat <- function(name = "", cat = T) {
   
   #install.packages("cowsay")
   #library(cowsay)
   
-  quotes <- c("I do declare. You are officially awesome at this.",
-              "Keep going!",
-              "All that hard work is paying off!",
-              "You are quite good my friend!",
+  quotes <- c("The party starts meow!",
+              "I do declare. You are officially pawesome at this.",
+              "Keep clawing forward.",
+              "All that work is paying off.",
+              "You are quite good my friend.",
               "You are on a roll.",
               "Excellently done!",
               "Keep the streak going.",
               "You are unstoppable.",
-              "Meow meow!",
-              "You're making your teachers proud.",
-              "You have serious R skills.",
+              "MeoWowwwwwwwwwwwww!",
+              "I am proud of you.",
+              "You have furry serious R skills.",
               "That was nicely done.",
-              "You are on your way.",
-              "Wow! Great job!",
-              "My pet chicken says you're Eggsellent!",
+              "Way to land on your feet!",
+              "Wow! Great job! Now feed me.",
+              "You have the eye of the tiger.",
               "Keep being amazing!",
               "You're on fire!",
               "That was purrfect!",
-              "You are too good. Let's paws for a moment and appreciate your greatness.",        
-              "Great job! Fur real!",
-              "You sure aren't kitten around! Great work!",
+              "Allow me to paws for a moment to appreciate your greatness.",        
+              "Great job! Furr real!",
+              "You aren't kitten around!",
               "Nothing gives you paws.",
-              "R you feline fine after that exercise?",
+              "Time for less worry and more purry.",
+              "I'm feline good about your code.",
               "You are a catalyst for success!",
               "Breaking Meows! You rock!",
               "Not too shabby for a tabby!",
               "Way to dig your claws in!",
+              "Not bad. That wasn't a total catastrophe.",
               "You are the whole kitten-kaboodle."
               )
   
@@ -42,7 +49,7 @@ encourage_cat <- function(name = "", cat = T) {
   # Print to screen
   if (cat) {
     
-    back_cat <-' \n -----------------------------
+    cats <- c(back_cat = ' \n ------------------------------
         \u005c
          \u005c
           \u005c
@@ -57,15 +64,80 @@ encourage_cat <- function(name = "", cat = T) {
       jgs   \u005c__  _/
               ( (
                ) )
-              (_('
+              (_(',
+    longcat = paste0('\n ------------------------------ \n    \\\   \n     \\\
+    .\uFF8A,,\uFF8A
+    ( \uFF9F\u03C9\uFF9F)
+    |\u3064  \u3064\n',
+paste0(rep("    |    |\n", 14), collapse = ""),
+    '    U "  U
+               [BoingBoing]
+    '),
+    stretchycat =
+      '\n ------------------------------
+      \u005c
+       \u005c
+        \u005c
+                        ,/\u007c         _.--\u201B\u201B^``-...___.._.,;
+                      /, \u005c\u201B.     _-\u201B          ,--,,,--\u201B\u201B\u201B
+                     {  \u005c    `_-\u201B\u201B       \u201B    /}\u201B
+Jill                    `;;\u201B             ;   ; ;
+                  ._.--\u201B\u201B     ._,,, _..\u201B  .;.\u201B
+                  (,_....----\u201B\u201B\u201B     (,..--\u201B\u201B
+  ',
+    anxiouscat =
+      '\n ------------------------------
+      \u005c
+       \u005c
+        \u005c
+        /\u005c_/\u005c         _
+       /``   \u005c       / )
+       \u007cn n   \u007c__   ( (
+      =(Y =.\u201B`   `\u005c  \u005c \u005c
+      {`"`        \u005c  ) )
+      {       /    \u007c/ /
+       \u005c\u005c   ,(     / /
+        ) ) /-\u201B\u005c  ,_.\u201B
+  jgs  (,(,/ ((,,/
+  ',
+    longtailcat =
+      '\n ------------------------------
+      \u005c
+       \u005c
+        \u005c
+     /\u005c-/\u005c
+    /a a  \u005c                                 _
+   =\u005c Y  =/-~~~~~~-,_______________________/ )
+     \u201B^--\u201B          ________________________/
+       \u005c           /
+       \u007c\u007c  \u007c---\u201B\u005c  \u005c
+  jgs  (_(__\u007c   ((__\u007c
+  ', 
+  cat = '\n ------------------------------
+      \u005c
+       \u005c
+        \u005c
+            \u007c\u005c___/\u007c
+          ==) ^Y^ (==
+            \u005c  ^  /
+             )=*=(
+            /     \u005c
+            \u007c     \u007c
+           /\u007c \u007c \u007c \u007c\u005c
+           \u005c\u007c \u007c \u007c_\u007c/\u005c
+      jgs  //_// ___/
+               \u005c_)
+  ')
+    
+    rando_cat <- sample(cats, 1)[[1]]
     
     if (nchar(name) > 0) {
-      return(cat(paste0("\n Meow-meow ", name, ". ", quote, back_cat)))
+      return(cat(paste0("\n Meow-meow ", name, ". ", quote, rando_cat)))
     }
     
-    return(cat(paste0("\n Meow-meow. ", quote, back_cat)))
+    return(cat(paste0("\n Meow-meow. ", quote, rando_cat)))
   }
   
-  return(cat(paste0("\n ", quote))) 
+  return(cat(paste0("\n ", quote)))
+  
 }
-
